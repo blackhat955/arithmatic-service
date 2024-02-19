e706bebef7627376b5d4c550388c942958972a3b
jkkatari <jkkatari@iu.edu>
2024-02-19 09:32:38 -0500

    test update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require ('express');
 const cors = require ('cors');
+const { add, sub, mul, div } = require("./arithmetica");
 const app = express();
 app.use(cors());
 const port = 3000;
+
 app.get('/', (req, res) => {
     res.send('Arithetic Service - Hello World');
 });
@@ -10,29 +12,29 @@ app.get('/', (req, res) => {
 app.get('/add/:n/:m', (req, res) => {
     let n = Number(req.params.n);
     let m = Number(req.params.m);
-    let sum = n + m;
+    let sum = add(n,m);
     res.json(sum);
 })
 
-// app.get('/add/:n/:m', (req, res) => {
-//     let n = Number(req.params.n);
-//     let m = Number(req.params.m);
-//     let sb = sub(n,m);
-//     res.json(sb);
-// })
+app.get('/sub/:n/:m', (req, res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let sb = sub(n,m);
+    res.json(sb);
+})
 
-// app.get('/add/:n/:m', (req, res) => {
-//     let n = Number(req.params.n);
-//     let m = Number(req.params.m);
-//     let mlt = mul(n,m);
-//     res.json(mlt);
-// })
+app.get('/mul/:n/:m', (req, res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let mlt = mul(n,m);
+    res.json(mlt);
+})
 
-// app.get('/add/:n/:m', (req, res) => {
-//     let n = Number(req.params.n);
-//     let m = Number(req.params.m);
-//     let dv = div(n, m);
-//     res.json(dv);
-// })
+app.get('/div/:n/:m', (req, res) => {
+    let n = Number(req.params.n);
+    let m = Number(req.params.m);
+    let dv = div(n, m);
+    res.json(dv);
+})
 
 app.listen(port);
