7e72d5f7abcfe75a1bd4e2fe0ae514412d293421
Durgesh Tiwari <61361185+blackhat955@users.noreply.github.com>
2024-04-16 16:15:48 -0400

    latest one

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
-const express = require ('express');
-const cors = require ('cors');
-const { add, sub, mul, div } = require("./arithmetica");
-const app = express();
-app.use(cors());
-const port = 3000;
 
-// wowrking with the routes for  here 
+
+
+
+
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors'); 
+const { add } = require("./arithmetica");
+const app = express();
+app.use(cors()); 
+
+if (!process.env.PORT) {
+  throw new Error(`Please specify the 
+  port number for the HTTP server with the environment variable PORT.`);
+}
+
+
+
+const port = process.env.PORT; 
+app.get('/', (req, res) => {
+  res.send('Arithmetic service - last updated 3/4/2022');
+}); 
+
 app.get('/', (req, res) => {
     res.send('Arithetic Service - Hello World');
 });
-
 app.get('/add/:n/:m', (req, res) => {
-    let n = Number(req.params.n);
-    let m = Number(req.params.m);
-    let sum = add(n,m);
+  let n = Number(req.params.n);
+  let m = Number(req.params.m);
+  let sum = add(n, m);
     res.json(sum);
-})
+  }); 
 
-app.get('/sub/:n/:m', (req, res) => {
-    let n = Number(req.params.n);
-    let m = Number(req.params.m);
-    let sb = sub(n,m);
-    res.json(sb);
-})
-
-app.get('/mul/:n/:m', (req, res) => {
+  app.get('/mul/:n/:m', (req, res) => {
     let n = Number(req.params.n);
     let m = Number(req.params.m);
     let mlt = mul(n,m);
@@ -37,5 +45,5 @@ app.get('/div/:n/:m', (req, res) => {
     let dv = div(n, m);
     res.json(dv);
 })
-
+ 
 app.listen(port);
